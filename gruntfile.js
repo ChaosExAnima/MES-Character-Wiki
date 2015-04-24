334535c90e6159188a6d96b1cba91cc16a5b2c0f
Ephraim Gregor <ephraimgregor@gmail.com>
2015-04-24 19:23:49 -0400

    Initial commit.

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+/**
+ * Grunt Wordpress helper and validator.
+ * @author Ephraim Gregor
+ * @copyright USATODAY Sports Media Group
+ * @package lawrence-dev-tools
+ */
+
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+		watch: {
+			options: {
+				spawn: false,
+				livereload: true
+			},
+			stylus: {
+				files: [ 'stylus/**/*.styl', 'mail.html' ],
+				tasks: [ 'stylus', 'csslint' ]
+			}
+		},
+
+		stylus: {
+			compile: {
+				options: {
+					'include css': true,
+					compress: false,
+					import: [ 'nib' ]
+				},
+				files: {
+					'style.css' : 'stylus/style.styl'
+				}
+			}
+		},
+		csslint: {
+			strict: {
+				options: {
+					csslintrc: '.csslintrc',
+					import: 2
+
+				},
+				src: [ 'style.css' ]
+			}
+		}
+	});
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	grunt.option( 'force', true );
+
+	grunt.registerTask( 'default', [ 'watch' ] );
+	grunt.registerTask( 'compile', [ 'stylus' ] );
+};
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="client-nojs">
+<head>
+	<title><?php echo basename( __DIR__ ); ?> - Mind's Eye Society Wiki</title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="http://wiki.mindseyesociety.org/load.php?debug=false&amp;lang=en&amp;modules=mediawiki.legacy.commonPrint%2Cshared%7Cskins.vector&amp;only=styles&amp;skin=vector&amp;*" />
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body class="mediawiki ltr sitedir-ltr ns-0 ns-subject page-<?php echo basename( __DIR__ ); ?> skin-vector action-view vector-animateLayout">
+	<div id="content" class="mw-body">
+		<h1 id="firstHeading" class="firstHeading"><span dir="auto"><?php echo basename( __DIR__ ); ?></span></h1>
+		<div id="bodyContent">
+			<div id="siteSub">From Mind&#039;s Eye Society Wiki</div>
+			<div id="contentSub"></div>
+			<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">
+				<?php include 'character.html'; ?>
+			</div><!-- /bodycontent -->
+		</div>
+	</div>
+	<div id="mw-head" class="noprint"></div>
+	<div id="mw-panel" class="noprint"></div>
+	<div id="footer"></div>
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+</html>
